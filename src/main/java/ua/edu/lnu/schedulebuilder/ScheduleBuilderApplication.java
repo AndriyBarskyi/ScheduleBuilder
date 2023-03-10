85599e0bd26d826597a206886ff1117aead019af
AndriyBarskyi <39509993+AndriyBarskyi@users.noreply.github.com>
2023-03-10 13:02:31 +0200

    Create initial version

---
+++ src/main/java/ua/edu/lnu/schedulebuilder/ScheduleBuilderApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.lnu.schedulebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/lnu/schedulebuilder/ScheduleBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.lnu.schedulebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
