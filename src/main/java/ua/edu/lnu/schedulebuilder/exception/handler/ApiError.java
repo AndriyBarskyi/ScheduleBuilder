1268abb4e1aba81ab77b35ee4ea4a53926d98493
AndriyBarskyi <39509993+AndriyBarskyi@users.noreply.github.com>
2023-04-23 14:34:18 +0300

    clean custom exception handler

--- src/main/java/ua/edu/lnu/schedulebuilder/exception/handler/ApiError.java
+++ src/main/java/ua/edu/lnu/schedulebuilder/exception/handler/ApiError.java
@@ -17,7 +17,6 @@ class ApiError {
     private LocalDateTime timestamp;
     private String message;
     private String debugMessage;
-    private List<ApiSubError> subErrors;
 
     private ApiError() {
         timestamp = LocalDateTime.now();
@@ -26,19 +25,5 @@ class ApiError {
     ApiError(HttpStatus status) {
         this();
         this.status = status;
-    }
-
-    ApiError(HttpStatus status, Throwable ex) {
-        this();
-        this.status = status;
-        this.message = "Unexpected error";
-        this.debugMessage = ex.getLocalizedMessage();
-    }
-
-    ApiError(HttpStatus status, String message, Throwable ex) {
-        this();
-        this.status = status;
-        this.message = message;
-        this.debugMessage = ex.getLocalizedMessage();
     }
 }
